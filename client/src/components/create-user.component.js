2458a7f0d6942a0a44c58d944a87ef1ed3c8b445
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-09 09:32:41 -0400

    quick fixes

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -24,7 +24,7 @@ export default class CreateUser extends Component {
     // axios.post takes two arguments
     // first: the url to post to as a string
     // Second: what we are posting
-    axios.post(BACKEND_URL + 'users/add' + newUser)
+    axios.post(BACKEND_URL + 'users/add' , newUser)
     .then((res) => {
         console.log(res.data)
     });

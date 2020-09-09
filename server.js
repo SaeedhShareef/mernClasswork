9bc9d2919b3effd464a9bc0186a7716c1e474a53
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-09 11:24:59 -0400

    Heroku

--- server.js
+++ server.js
@@ -1,52 +1,36 @@
-const express = require('express')
-
-const cors = require('cors')
-
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose')
 
+
+require('dotenv').config();
+
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-// allows us to have environment variables in the .env file
-
-require('dotenv').config()
-
-// start middleware
-
-const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(express.static('client/build'));
 
-
-// end middleware
-
-const uri = process.env.ATLAS_URI
-mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true});
-
+const uri = process.env.ATLAS_URI;
+        
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 
+
 connection.once('open', () => {
-    console.log('mongodb database connection established succesfully')
+  console.log("MongoDB database connection established successfully");
 })
 
-// start Routes
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users')
-app.use('/exercises', exercisesRouter)
-app.use('/users', usersRouter)
-// end Routes
+const exercisesRouter = require('./routes/exercises');  
+const usersRouter = require('./routes/users');  
 
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/client/build/index.html', function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
-
+app.use('/exercises', exercisesRouter);  
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-    console.log(`server is running on port: ${port}`)
-})
+console.log(`Server is running on port: ${port}`);
+});
